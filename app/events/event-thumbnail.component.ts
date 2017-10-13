4de9b50cecf5abcdd4944d539342f03863db9648
James Abney <james.abney@gmail.com>
2017-10-12 17:33:02 -0700

    Add event thumbnail styles

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { EventsListComponent } from './events-list.component';
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core'
+import { EventsListComponent } from './events-list.component'
 
 @Component({
   selector: 'event-thumbnail',
@@ -8,17 +8,19 @@ import { EventsListComponent } from './events-list.component';
       <h2>{{event.name}}</h2>
       <div>Date: {{event.date}}</div>
       <div>Time: {{event.time}}</div>
-      <div>Price: \${{ event.price }}</div>
+      <div>Price: \${{event.price}}</div>
       <div>
         <span>Location: {{event.location.address}}</span>
-        <span>&nbsp;</span>
-        <span>{{event.location.city}}, {{event.location.country}}</span>
+        <span class="pad-left">{{event.location.city}}, {{event.location.country}}</span>
       </div>
       <ng-content></ng-content>
     </div>
-  `
+  `,
+  styles: [`
+    .pad-left { margin-left: 0.5em; }
+    .well div { color: #bbb; }
+  `]
 })
-
 export class EventThumbnailComponent implements OnInit {
   @Input() event: any
 
