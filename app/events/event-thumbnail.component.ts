21ddf4a19b1939c8566ca4f279981faeda6c36cd
James Abney <james.abney@gmail.com>
2017-10-13 16:48:27 -0700

    Add ngSwitch to time (early, late start)

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -7,7 +7,12 @@ import { EventsListComponent } from './events-list.component'
     <div class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
-      <div>Time: {{event?.time}}</div>
+      <div [ngSwitch]="event?.time">
+        Time: {{event?.time}}
+        <span *ngSwitchCase="'8:00 am'">(Early start)</span>
+        <span *ngSwitchCase="'10:00 am'">(Late start)</span>
+        <span *ngSwitchDefault>(Normal start)</span>
+      </div>
       <div>Price: \${{event?.price}}</div>
       <div *ngIf="event?.location">
         <span>Location: {{event?.location?.address}}</span>
