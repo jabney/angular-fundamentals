829e851e4d64c935df3159cbb1fb5eb0f511e3fa
James Abney <james.abney@gmail.com>
2017-10-14 16:28:20 -0700

    Add routerLink attribute to thumbail div container

--- app/events/event-thumbnail.component.ts
+++ app/events/event-thumbnail.component.ts
@@ -4,7 +4,7 @@ import { EventsListComponent } from './events-list.component'
 @Component({
   selector: 'event-thumbnail',
   template: `
-    <div class="well hoverwell thumbnail">
+    <div [routerLink]="['/events', event.id]" class="well hoverwell thumbnail">
       <h2>{{event?.name}}</h2>
       <div>Date: {{event?.date}}</div>
       <div [ngStyle]="getStartTimeStyle(event)" [ngSwitch]="event?.time">
