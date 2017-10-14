234a579ff45525839243a227f3c41a7b56e5b5df
James Abney <james.abney@gmail.com>
2017-10-14 16:25:49 -0700

    Cleanup

--- app/events/event-details/event-details.component.ts
+++ app/events/event-details/event-details.component.ts
@@ -3,7 +3,6 @@ import { EventService } from '../shared/event.service';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  // selector: 'event-details',
   templateUrl: 'app/events/event-details/event-details.component.html',
   styles: [`
     .container { padding-left: 20px; padding-right: 20px; }
