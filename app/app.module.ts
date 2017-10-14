c97ecd9a76583e1119d8babb162db76ac6cd33e5
James Abney <james.abney@gmail.com>
2017-10-14 12:51:23 -0700

    Import and provide EventService and ToastrService

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,6 +4,8 @@ import { EventsAppComponent } from './events-app.component'
 import { EventsListComponent } from './events/events-list.component'
 import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
+import { EventService } from './events/shared/event.service'
+import { ToastrService } from './common/toastr.service'
 
 @NgModule({
   imports: [ BrowserModule ],
@@ -15,6 +17,9 @@ import { NavbarComponent } from './nav/navbar.component'
     EventThumbnailComponent
   ],
   bootstrap: [ EventsAppComponent ],
-  providers: [],
+  providers: [
+    EventService,
+    ToastrService
+  ],
 })
 export class AppModule { }
