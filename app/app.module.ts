7ca75ff3d86ae59f2cb423a5c79cdcfe4fe5ea59
James Abney <james.abney@gmail.com>
2017-10-15 13:13:08 -0700

    Group events folder imports into barrels

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,18 +2,22 @@ import { NgModule } from '@angular/core'
 import { RouterModule } from '@angular/router'
 import { BrowserModule } from '@angular/platform-browser'
 
-import { EventService } from './events/shared/event.service'
-import { ToastrService } from './common/toastr.service'
-import { EventRouteActivatorService } from './events/event-details/event-route-activator.service'
-
 import { EventsAppComponent } from './events-app.component'
-import { EventsListComponent } from './events/events-list.component'
-import { EventThumbnailComponent } from './events/event-thumbnail.component'
 import { NavbarComponent } from './nav/navbar.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event.component'
 import { Error404Component } from './errors/404.component'
-import { EventsListResolverService } from './events/events-list-resolver.service'
+
+import {
+  EventService,
+  EventRouteActivatorService,
+  EventsListComponent,
+  EventThumbnailComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventsListResolverService
+} from './events/index'
+
+import { ToastrService } from './common/toastr.service'
+
 import { appRoutes } from './routes'
 
 @NgModule({
--- app/routes.ts
+++ app/routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router'
-import { EventsListComponent } from './events/events-list.component'
-import { EventDetailsComponent } from './events/event-details/event-details.component'
-import { CreateEventComponent } from './events/create-event.component'
+
+import {
+  EventRouteActivatorService,
+  EventsListComponent,
+  EventDetailsComponent,
+  CreateEventComponent,
+  EventsListResolverService
+} from './events/index'
+
 import { Error404Component } from './errors/404.component'
-import { EventRouteActivatorService } from './events/event-details/event-route-activator.service'
-import { EventsListResolverService } from './events/events-list-resolver.service'
 import { UserModule } from './user/user.module'
 
 export const appRoutes: Routes = [
