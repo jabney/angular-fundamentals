e7b43f80245f12b12ada21a36dbac983c63aaab4
James Abney <james.abney@gmail.com>
2017-10-16 15:52:52 -0700

    Implement profile form using reactive forms

--- app/user/profile.component.ts
+++ app/user/profile.component.ts
@@ -1,18 +1,51 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormControl, FormGroup, Validators } from '@angular/forms'
+import { AuthService } from './auth.service';
+import { Router } from '@angular/router';
 
 @Component({
-  template: `
-    <h1>Edit Your Profile</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Edit profile form will go here]</h3>
-      <br />
-      <br />
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default">Cancel</button>
-    </div>
-  `,
+  templateUrl: 'app/user/profile.component.html',
+  styles: [`
+    em { float: right; color: #e05c65; padding-left: 10px; }
+    .error input { background-color: #e3c3c5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-input-placeholder { color: #999; }
+    .error :-moz-input-placeholder { color: #999; }
+    .error :-ms-input-placeholder { color: #999; }
+  `]
 })
-export class ProfileComponent {
+export class ProfileComponent implements OnInit {
+  public profileForm: FormGroup
 
+  constructor(
+    private _as: AuthService,
+    private _router: Router
+  ) { }
+
+  public ngOnInit() {
+    const firstName = new FormControl(
+      this._as.currentUser.firstName,
+      Validators.required)
+
+    const lastName = new FormControl(
+      this._as.currentUser.lastName,
+      Validators.required)
+
+    this.profileForm = new FormGroup({ firstName, lastName })
+  }
+
+  public cancel() {
+    this._router.navigate(['/events'])
+  }
+
+  public save(value: any) {
+    if (this.profileForm.valid) {
+      this._as.updateCurrentUser(value.firstName, value.lastName)
+      this._router.navigate(['/events'])
+    }
+  }
+
+  public controlIsValid(control: FormControl) {
+    return control.valid || control.untouched
+  }
 }
