0cad028fabcff1c4fa75468f3f823ab782477dd3
James Abney <james.abney@gmail.com>
2017-10-15 17:23:18 -0700

    Implement login form validation

--- app/user/login.component.ts
+++ app/user/login.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from './auth.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'login',
-  templateUrl: 'app/user/login.component.html'
+  templateUrl: 'app/user/login.component.html',
+  styles: [`
+    em { float: right; color: #e05c65}
+  `]
 })
 
 export class LoginComponent implements OnInit {
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _as: AuthService
+  ) { }
 
   ngOnInit() { }
 
-  login(formValues: any) {
-    console.log(formValues)
+  login(values: any) {
+    this._as.loginUser(values.userName, values.password)
+    this._router.navigate(['/events'])
+  }
+
+  cancel() {
+    this._router.navigate(['/events'])
+  }
+
+  isInvalid(control) {
+    return control && (control.touched || control.dirty) && control.invalid
   }
 }
