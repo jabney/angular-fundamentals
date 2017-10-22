826f509652442d73a87a360b7249a16be4ba8e32
James Abney <james.abney@gmail.com>
2017-10-22 13:40:35 -0700

    Implement create-event component

--- app/events/create-event.component.ts
+++ app/events/create-event.component.ts
@@ -1,31 +1,77 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, ViewChild, AfterViewInit, OnDestroy } from '@angular/core'
 import { Router } from '@angular/router'
+import { EventService } from './shared/event.service'
+import { IEvent } from './shared/event.model'
+import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs/Subscription';
 
 @Component({
-  template: `
-    <h1>New Event</h1>
-    <hr>
-    <div class="col-md-6">
-      <h3>[Create Event form will go here]</h3>
-      <br>
-      <br>
-      <button type="submit" class="btn btn-primary">Save</button>
-      <button type="button" class="btn btn-default"
-        (click)="cancel()"
-        >Cancel
-      </button>
-    </div>
-  `
+  templateUrl: 'app/events/create-event.component.html',
+  styles: [`
+    em { float: right; color: #e05c65; padding-left: 10px; }
+    .error input { background-color: #e3c3c5; }
+    .error ::-webkit-input-placeholder { color: #999; }
+    .error ::-moz-input-placeholder { color: #999; }
+    .error :-moz-input-placeholder { color: #999; }
+    .error :-ms-input-placeholder { color: #999; }
+`]
 })
 
-export class CreateEventComponent implements OnInit {
-  public isDirty = false
+export class CreateEventComponent implements OnInit, AfterViewInit, OnDestroy {
+  // public event: IEvent
+  @ViewChild('newEventForm') public form: NgForm
 
-  constructor(private _router: Router) { }
+  private _isDirty = false
+  private _changesSub: Subscription
 
-  ngOnInit() { }
+  constructor(
+    private _router: Router,
+    private _es: EventService
+  ) {}
 
-  cancel() {
+  get isDirty(): boolean {
+    return this._isDirty
+  }
+
+  ngOnInit() {
+    // this.event = {
+    //   id: 999,
+    //   name: 'Ng Spectacular',
+    //   date: new Date('8/8/2018'),
+    //   time: '10:00 am',
+    //   price: 99.99,
+    //   location: {
+    //     address: '1234 5th St',
+    //     city: 'Seattle, WA',
+    //     country: 'USA'
+    //   },
+    //   sessions: [],
+    //   onlineUrl: 'http://ngSpectacular.com',
+    //   imageUrl: 'http://ngSpectacular.com/logo.png'
+    // }
+  }
+
+  ngAfterViewInit() {
+    this._changesSub = this.form.valueChanges.subscribe((value: any) => {
+      this._isDirty = true
+    })
+  }
+
+  cancel(): void {
     this._router.navigate(['/events'])
+  }
+
+  saveEvent(formValue: IEvent): void {
+    this._es.saveEvent(formValue)
+    this._isDirty = false
+    this._router.navigate(['/events'])
+  }
+
+  setDirty(): void {
+    this._isDirty = true
+  }
+
+  ngOnDestroy() {
+    this._changesSub.unsubscribe()
   }
 }
