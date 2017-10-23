1a0133b89c4208aa1dec1f9c94f8597d67cb6c13
James Abney <james.abney@gmail.com>
2017-10-23 14:46:55 -0700

    Implement session search functionality

--- app/nav/navbar.component.ts
+++ app/nav/navbar.component.ts
@@ -1,19 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { AuthService } from '../user/auth.service';
+import { Component, OnInit } from '@angular/core'
+import { AuthService } from '../user/auth.service'
+import { ISession, EventService } from '../events/index'
 
 @Component({
   selector: 'navbar',
   templateUrl: 'app/nav/navbar.component.html',
   styles: [`
     .nav.navbar-nav { font-size: 15px; }
-    #searchForm { margin-right: 100px; }
-    @media (max-width: 1200px) { #searchForm { display: none; } }
+    // #searchForm { margin-right: 100px; }
+    // @media (max-width: 992px) { #searchForm { display: none; } }
     li > a.active { color: #f97924}
   `]
 })
 
 export class NavbarComponent implements OnInit {
-  constructor(private _as: AuthService) { }
+  public searchTerm: string = ''
+  public foundSessions: ISession[]
+
+  constructor(
+    private _as: AuthService,
+    private _es: EventService
+  ) { }
 
   ngOnInit() { }
 
@@ -25,4 +32,10 @@ export class NavbarComponent implements OnInit {
     return this._as.isAuthenticated()
   }
 
+  searchSessions(term) {
+    this._es.searchSessions(term).subscribe((sessions) => {
+      this.foundSessions = sessions
+      console.log(this.foundSessions)
+    })
+  }
 }
