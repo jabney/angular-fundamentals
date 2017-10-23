ae13ff5c296c69516e04155af4c0e64b2e276d16
James Abney <james.abney@gmail.com>
2017-10-23 12:28:37 -0700

    Add map operator

--- app/events/events-list-resolver.service.ts
+++ app/events/events-list-resolver.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core'
 import { Resolve } from '@angular/router'
 import { EventService } from './shared/event.service'
 import { IEvent } from './index'
+import 'rxjs/add/operator/map'
 
 @Injectable()
 export class EventsListResolverService implements Resolve<IEvent[]> {
