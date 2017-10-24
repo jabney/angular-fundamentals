8b24a67f6c663cb7dd45fd8986dda1aa901bbf49
James Abney <james.abney@gmail.com>
2017-10-24 09:46:29 -0700

    Implement modal close on click

--- app/common/simple-modal.component.ts
+++ app/common/simple-modal.component.ts
@@ -1,16 +1,17 @@
-import { Component, OnInit, Input } from '@angular/core'
+import { Component, OnInit, Input, Inject, ViewChild, ElementRef } from '@angular/core'
+import { JQ_TOKEN } from './jquery.service'
 
 @Component({
   selector: 'simple-modal',
   template: `
-    <div id="{{elementId}}" class="modal fade" tabIndex="-1">
+    <div id="{{elementId}}" #modal class="modal fade" tabIndex="-1">
       <div class="modal-dialog">
         <div class="modal-content">
           <div class="modal-header">
             <button type="button" class="close" data-dismiss="modal"><span>&times;</span></button>
             <h4 class="modal-title">{{title}}</h4>
           </div>
-          <div class="modal-body">
+          <div class="modal-body" (click)="closeModal()">
             <ng-content></ng-content>
           </div>
         </div>
@@ -25,8 +26,21 @@ import { Component, OnInit, Input } from '@angular/core'
 export class SimpleModalComponent implements OnInit {
   @Input() public title: string
   @Input() public elementId: string
+  @Input() public closeOnClick: string
 
-  constructor() { }
+  @ViewChild('modal') private _modalRef: ElementRef
 
-  ngOnInit() { console.log('elementId:', this.elementId)}
+  private get _modal(): HTMLElement {
+    return this._modalRef.nativeElement
+  }
+
+  constructor(@Inject(JQ_TOKEN) private $: any) { }
+
+  ngOnInit() { }
+
+  closeModal() {
+    if (this.closeOnClick.toLocaleLowerCase() === 'true') {
+      this.$(this._modal).modal('hide')
+    }
+  }
 }
